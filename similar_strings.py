fc05053dd11cbff065df5b2d4dd5375a22624af7
Maltesh GM <zizou5mal@gmail.com>
2016-11-22 23:14:46 +0530

    update

--- similar_strings.py
+++ similar_strings.py
@@ -1,4 +1,19 @@
 #https://www.hackerrank.com/challenges/similar-strings
+
+def comp(x,y):
+    for i in range(len(x)):
+        for j in range(len(x)):
+            if i !=j:
+                if  x[i]==x[j]:
+                    if y[i] != y[j]:
+                        return False
+                        break
+                else:
+                    if y[i] == y[j]:
+                        return False
+                        break
+    return True
+
 n,q = map(int, raw_input().strip().split(' '))
 S = raw_input().strip()
 while q > 0:
@@ -7,30 +22,14 @@ while q > 0:
     slic = S[l:r]
     if l > 0:
         patern = S[l-1:r]
-
         i = 0
         diff = r - l + 1
-        real_pat = [ fl == sl for fl,sl in zip(patern,patern[1:]) ]
-        print patern
+        real_pat = [ fl == sl for fl,sl in zip(patern,patern[1:])]
         all_paterns = [S[i:i+diff]  for i  in range(0,len(S)) if len(patern) == len(S[i:i+diff])]
-        print all_paterns
         yyeah = False
         for ptrn in all_paterns:
-            for i in range (len(patern)):
-                for j in range(len(ptrn)):
-                  if i<>j:
-                    if  patern[i] == patern[j]:
-                        if ptrn[i] <>  ptrn[j]:
-                            yyeah = False
-                            break
-                    else:
-                        if ptrn[i] == ptrn[j]:
-                            yyeah = False
-                            break
-                    yyeah = True
-            if yyeah:
+            if comp(ptrn,patern):
                 cont = cont + 1
-                yyeah = False
     print cont
-
     q = q - 1
+
