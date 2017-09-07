266bdfe93a04debbf3876ae8d630b86652a7c346
Maltesh GM <zizou5mal@gmail.com>
2017-09-08 00:41:01 +0530

    Update smallest_sum.py
    Better approach

--- smallest_sum.py
+++ smallest_sum.py
@@ -1,16 +1,32 @@
 #https://www.codewars.com/kata/smallest-possible-sum
 
-def solution(lst):
-    while True:
-        max_ = max(lst)
-        min_ = min(lst)
-        mx_idx = lst.index(max_)
-        mn_idx = lst.index(min_)
-        if lst.count(lst[0]) == len(lst) :
-            return sum(lst)
-            break            
-        elif lst[mx_idx]>lst[mn_idx]:            
-            lst[mx_idx] =lst[mx_idx] - lst[mn_idx]
+# def solution(lst):
+#     while True:
+#         max_ = max(lst)
+#         min_ = min(lst)
+#         mx_idx = lst.index(max_)
+#         mn_idx = lst.index(min_)
+#         if lst.count(lst[0]) == len(lst) :
+#             return sum(lst)
+#             break            
+#         elif lst[mx_idx]>lst[mn_idx]:            
+#             lst[mx_idx] =lst[mx_idx] - lst[mn_idx]
 
+
+def GCD (a,b):
+    if b==0:
+        return a;
+    return GCD(b,a%b)
+
+
+def solution(lst):
+    ct = len(lst)
+    ele = lst[0]
+    lst = iter(lst[1:])
+    for item in lst:
+        ele = GCD(item,ele)
+    return int(ele * ct)
+        
+    
 
 print solution ([1, 21, 55])
