e6af80742b145ac9cdf50f3dc5ed25f7e75e02b4
Maltesh GM <zizou5mal@gmail.com>
2018-05-22 00:17:30 +0530

    Add files via upload

--- js/bubble_sort.js
+++ js/bubble_sort.js
@@ -1,8 +1,21 @@
 /*
-
+Bruteforce Application for sorting problem is to compare adjacenet elements of the list and exchange 
+them if they are ourt of order.By doing repatedly we end up "bubbling up" largest lement to last position 
+on the list
  */
 
 var bubble_sort = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                let temp = arr[j];
+                arr[j] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+    return arr
 
 }
 
