b215f510c1bac535d8d55f9ebfbed29237f44f56
Maltesh GM <zizou5mal@gmail.com>
2018-05-29 00:54:32 +0530

    Add files via upload

--- Priori_Incantatem.js
+++ Priori_Incantatem.js
@@ -1,91 +1,29 @@
-// https://www.codewars.com/kata/priori-incantatem/train/javascript
-// class Wand{
-
-//     //  let my_stack:Array<any>=new Array<any>;
-//      constructor(spells){
-//          if (spells){
-//              Object.keys(spells).forEach(element => {
-//                 this[element] =>{ 
-//                     this.my_stack.push(element);
-//                     return spells[element];
-//                 }
-//              });
-//          }
-//     }
-
-//     prioriIncantatem(){
-//     }
-
-//     deletrius(){
-//     }
-// }
-
-// function Wand(spells){
-//     this.max_stack= ['test'];
-//     if (spells){
-//        Object.keys(spells).forEach(element => {
-//          this[element] = spells[element];
-//        });
-//     }
-// }
-// Wand.prototype.get = function(obj,prop){
-//   console.log(obj,prop,this);
-//   if (this.hasOwnProperty(prop)){
-//     this.max_stack.push(prop)
-//   }
-  
-// }
-// Wand.prototype.prioriIncantatem=function(){
-//   return this.max_stack;
-  
-// }
-// Wand.prototype.deletrius=function(){
-//   this.max_stack=[]
-// }
-
-// let spls = {
-//   alohomora: function() { console.log('unlocked!'); },
-//   lumos: function() { console.log('let there be light!'); },
-//   wingardiumLeviosa: function() { console.log('levitation!'); }
-// };
+//  https: //www.codewars.com/kata/priori-incantatem/train/javascriptconst handler = {
+//  https: //www.codewars.com/kata/priori-incantatem/train/javascriptconst handler = {
 
 
-// const Wand = new Proxy(Wand,{})
 
-const handler = {
-  construct(objTarget, args, oldConstructor) {
-     console.log(objTarget);
-     
-     
-     return new objTarget(...args)
-  },
-  set(target, key, value) {
-    console.log(`Setting value ${key} as ${value}`)
-    target[key] = value;
-  },
-  get(target, key) {
-    console.log(`Reading value from ${key}`)
-    return target[key];
-  },
-};
+const MAX_PRIOR_SPELLS = 3
+class Wand {
+    constructor(spells) {
+        this.my_stack = [];
+        this.ignore_methods = ['deletrius', 'my_stack'];
+        Object.assign(this, spells)
+        return new Proxy(this, {
+            get: function(target_obj, prop) {
+                if (target_obj.ignore_methods.indexOf(prop) === -1 &&
+                    typeof target_obj[prop] === 'function') {
+                    target_obj.my_stack.unshift(prop);
+                }
+                return target_obj[prop];
+            }
+        })
+    }
+    prioriIncantatem() {
+        return this.my_stack.slice(1, MAX_PRIOR_SPELLS + 1)
+    }
 
-function test(args){
-  if (args){
-    Object.keys(args).forEach((ele)=>{
-      this[ele] = args[ele];
-    })
-  }
+    deletrius() {
+        this.my_stack = ['deletrius']
+    }
 }
-
-
-let spls = {
-  alohomora: function() { console.log('unlocked!'); },
-  lumos: function() { console.log('let there be light!'); },
-  wingardiumLeviosa: function() { console.log('levitation!'); }
-};
-
-const Wand = new Proxy(test,handler);
-
-a = new Wand(spls);
-a.alohomora();
-console.log('121');
