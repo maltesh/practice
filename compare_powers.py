3010a30caf46d57ec5274b6ae48d9ab9f7436b96
Maltesh GM <zizou5mal@gmail.com>
2018-06-18 21:30:38 +0530

    Add files via upload

---
+++ compare_powers.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/compare-powers/python
+import math
+def compare_powers(n1,n2)    no1 = (int)(n1[1] * math.log10(n1[0])) + 1
+    no2 = (int)(n2[1] * math.log10(n2[0])) + 1
+    if no1 == no2:
+        return 0;
+    if no1 > no2:
+        return -1
+    else:
+        return 1
