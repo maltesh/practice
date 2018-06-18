705eca9cc9bc7dd174da9212e4517736776bb87d
Maltesh GM <zizou5mal@gmail.com>
2018-06-18 21:31:27 +0530

    Update compare_powers.py

--- compare_powers.py
+++ compare_powers.py
@@ -1,6 +1,7 @@
 #https://www.codewars.com/kata/compare-powers/python
 import math
-def compare_powers(n1,n2)    no1 = (int)(n1[1] * math.log10(n1[0])) + 1
+def compare_powers(n1,n2):
+    no1 = (int)(n1[1] * math.log10(n1[0])) + 1
     no2 = (int)(n2[1] * math.log10(n2[0])) + 1
     if no1 == no2:
         return 0;
