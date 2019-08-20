0f45bc0e3150b8bd89f50c54d31ebb302236f1f7
Maltesh GM <zizou5mal@gmail.com>
2019-08-20 23:28:09 +0530

    Easter Eggs

---
+++ 78B.py
@@ -0,0 +1,17 @@
+# http://codeforces.com/contest/78/problem/B
+
+print("ROY"+"GBIV"*30)[:int(raw_input())]
+
+# n = int(raw_input())
+# colors= ['R','O', 'Y', 'G', 'B','I','V']
+# q,r = divmod(n,7)
+# temp = []
+# i = 0
+# while i < n-1 :
+#     a = i % 4
+#     # print i,a , colors[a]
+#     temp.append(colors[a])
+#     i += 1
+    
+
+# print temp  
